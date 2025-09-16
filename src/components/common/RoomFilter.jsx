74c5270a7ccf6d3a3b40c257528f611a52d15a0b
Aleksa Djekic <84620255+Al3k5a24@users.noreply.github.com>
2025-09-16 23:16:38 +0200

    Room Filter

---
+++ src/components/common/RoomFilter.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const RoomFilter = (data,setFilteredData) => {
+    const[filter,setFilter]=useState("")
+
+    //function that will handle filtration
+    const handleSelectChange=(e)=>{
+        const selectedRoomType=e.target.
+        setFilter(selectedRoomType)
+
+        //set to lower case for accurate matching
+        const filteredRooms = data.filter((room)=>
+        room.roomType.toLowerCase().includes(selectedRoomType.toLowerCase()))
+        setFilteredData(filteredRooms)
+    }
+    
+    //if nothing is selected, return all data and display it
+    const clearFilter=()=>{
+        setFilter("")
+        setFilteredData(data)
+    }
+
+    //array of unique room types from 
+    const roomTypes=["", ...new Set(data.map(room=>room.roomType))]
+
+  return (
+    <div>
+      <span id="room-type-filter">Filter rooms by type</span>
+      <select
+      value={filter}
+      onChange={handleSelectChange}>
+        <option value={""}>Select a room type</option>
+        {roomTypes.map((type,index)=>(
+            <option key={index} value={String(type)}>
+                {type}
+            </option>
+        ))}
+      </select>
+      <button type="button" onClick={clearFilter}>
+        Clear Filter
+      </button>
+    </div>
+  )
+}
+
+export default RoomFilter
