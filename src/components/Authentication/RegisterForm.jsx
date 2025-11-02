ecc072fefc2b26e3d18e57a8b1bc97fe047fb414
Aleksa Djekic <84620255+Al3k5a24@users.noreply.github.com>
2025-11-02 20:25:01 +0100

    FE & BE create account feature completed

--- src/components/Authentication/RegisterForm.jsx
+++ src/components/Authentication/RegisterForm.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { createAccount } from "../../utils/ApiAuth";
 
 const RegisterForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [user, SetUser] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleFNInputChange = (e) => {
+    const value = e.target.value;
+    SetUser({ ...user, fullName: value });
+  };
+
+  const handleEmailInputChange = (e) => {
+    const value = e.target.value;
+    SetUser({ ...user, email: value });
+  };
+
+  const handlePassInputChange = (e) => {
+    const value = e.target.value;
+    SetUser({ ...user, password: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await createAccount(user);
+      if (response.status === 200) {
+        setSuccessMessage("Account successfully created!");
+        setErrorMessage("");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      } else {
+        setErrorMessage("Could not create account!");
+        setSuccessMessage("");
+      }
+    } catch (error) {
+      setErrorMessage(`Failed to book room: ${error.message}`);
+    }
+  };
+
   return (
     <div className="w-full max-w-lg max-h-full rounded-lg bg-gray-50 border border-gray-200 p-4 mx-2">
-      <form className="bg-[#F3EFE6] items-center py-4 px-3 rounded-3xl inset-shadow-sm inset-shadow-gray-400 drop-shadow-xl/40 space-y-3">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-[#F3EFE6] items-center py-4 px-3 rounded-3xl inset-shadow-sm inset-shadow-gray-400 drop-shadow-xl/40 space-y-3"
+      >
         <div className="py-4 flex justify-center">
           <a href="/">
             <img
@@ -19,12 +69,27 @@ const RegisterForm = () => {
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
           Create an account
         </h2>
+        {/* //displays success message if there is any */}
+        {successMessage && (
+          <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-3 w-full text-center">
+            {successMessage}
+          </div>
+        )}
+
+        {/* //displays error message if there is any */}
+        {errorMessage && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-3 w-full text-center">
+            {errorMessage}
+          </div>
+        )}
         <div className="flex flex-col items-start justify-start">
           <label htmlFor="email" className="mb-1 block text-sm text-gray-400">
             Full name:
           </label>
           <input
-            id="email"
+            id="fullName"
+            name="fullName"
+            onChange={handleFNInputChange}
             className="w-full border mt-1 border-gray-300
              bg-slate-100 mb-2 outline-none rounded-xl py-2.5 px-3
              placeholder-[#7f8c8d] text-gray-400 focus:ring-2 focus:ring-red-500 focus:outline-none"
@@ -40,6 +105,8 @@ const RegisterForm = () => {
           </label>
           <input
             id="email"
+            name="email"
+            onChange={handleEmailInputChange}
             className="w-full border mt-1 mb-2 border-gray-300
              bg-slate-100 outline-none rounded-xl py-2.5 px-3
              placeholder-[#7f8c8d] text-gray-400 focus:ring-2 focus:ring-red-500 focus:outline-none"
@@ -54,7 +121,9 @@ const RegisterForm = () => {
             Password:
           </label>
           <input
-            id="email"
+            id="password"
+            name="password"
+            onChange={handlePassInputChange}
             className="w-full border mt-1 border-gray-300
              bg-slate-100 mb-2 outline-none rounded-xl py-2.5 px-3
              placeholder-[#7f8c8d] text-gray-400 focus:ring-2 focus:ring-red-500 focus:outline-none"
@@ -65,7 +134,10 @@ const RegisterForm = () => {
         </div>
 
         <div className="flex items-center justify-center mt-5 mb-3">
-          <button className="w-1/2 mb-3 rounded-2xl bg-red-500 hover:bg-red-800 transition-all active:scale-95 py-2.5 text-white font-medium">
+          <button
+            type="submit"
+            className="w-1/2 mb-3 rounded-2xl bg-red-500 hover:bg-red-800 transition-all active:scale-95 py-2.5 text-white font-medium"
+          >
             Create Account
           </button>
         </div>
--- src/utils/ApiAuth.js
+++ src/utils/ApiAuth.js
@@ -5,3 +5,16 @@ import axios from "axios"
 export const api=axios.create({
     baseURL :"http://localhost:8080"
 })
+
+export async function createAccount(userData){
+try {
+    const formData=new FormData();
+    formData.append("fullName",userData.fullName);
+    formData.append("email",userData.email);
+    formData.append("password",userData.password);
+    const response=await api.post("/auth/create-account",formData)
+    return response
+} catch (error) {
+    throw new Error(`Could not create account ${error.error}`)
+}
+}
